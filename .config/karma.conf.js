657fd339aa18582c20921caf98bd0f6e98b36fdf
sagie gur ari <sagiegurari@gmail.com>
2021-10-17 12:42:18 +0000

    karma

---
+++ .config/karma.conf.js
@@ -0,0 +1,61 @@
+/*global module: false, require: false */
+
+module.exports = function (config) {
+    'use strict';
+
+    const mainJSFile = require('../package.json').main;
+
+    config.set({
+        basePath: '../',
+        frameworks: [
+            'mocha',
+            'sinon-chai'
+        ],
+        port: 9876,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        browsers: [
+            'ChromiumHeadless'
+        ],
+        singleRun: false,
+        reporters: [
+            'progress',
+            'coverage'
+        ],
+        preprocessors: {},
+        coverageReporter: {
+            dir: 'coverage',
+            reporters: [
+                {
+                    type: 'lcov',
+                    subdir: '.'
+                }
+            ],
+            check: {
+                global: {
+                    statements: 100,
+                    functions: 100,
+                    lines: 100,
+                    branches: 100
+                }
+            }
+        },
+        customLaunchers: {
+            ChromeHeadlessCI: {
+                base: 'ChromeHeadless',
+                flags: [
+                    '--no-sandbox'
+                ]
+            }
+        },
+        files: [
+            '**/big.js/big.js',
+            '**/jquery/dist/jquery.js',
+            '**/angular/angular.js',
+            '**/angular-mocks/angular-mocks.js',
+            'test/helpers/**/*.js',
+            mainJSFile,
+            'test/spec/**/*.js'
+        ]
+    });
+};
